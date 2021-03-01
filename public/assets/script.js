2165454fc295a0cd2a7c9495c89e2eeca87fd9eb
Jose Espinoza <jose20019004@gmail.com>
2021-03-01 13:05:59 -0500

    Added buttons for register and login

--- public/assets/script.js
+++ public/assets/script.js
@@ -2,14 +2,14 @@ const socket = io();
 
 //login funcitonality
 function login() {
-    var user = document.querySelector("#username").value;
-    var pass = document.querySelector("#password").value;
-    var question = document.querySelector("#question").value;
-    if(user == "lab18" && pass == "lab18") {
-        alert("Logged In");
+    var user = document.querySelector('#username').value;
+    var pass = document.querySelector('#password').value;
+    var question = document.querySelector('#question').value;
+    if (user === 'lab18' && pass === 'lab18') {
+        alert('Logged In');
         return false;
     } else {
-        alert("wrong user/pass");
+        alert('wrong user/pass');
         return false;
-        }
     }
+}
