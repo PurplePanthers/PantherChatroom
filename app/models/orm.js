dad186be8566e0a7739f779bc67491febfbe7983
John Murphy <murphbot55@gmail.com>
2021-03-02 12:57:44 -0500

    adding functions to orm

--- app/models/orm.js
+++ app/models/orm.js
@@ -7,38 +7,67 @@ We abstract our database and information-modelling code
 into this section
 ====================================================== */
 
-const db = require('../config/connection.js')('chats_db', 'joseantonio')
+const db = require("../config/connection.js")("chats_db", "joseantonio");
 // an external npm package we are using
-const moment = require('moment')
+const moment = require("moment");
 
-
-
-function getList(criteria = {}) {
-    return db.query('SELECT * FROM tasks ' + (criteria ? 'WHERE ? ' : ''), criteria)
+// returns all users
+function getAllUsers() {
+  return db.query("SELECT * FROM USERS");
+}
+// this info is what will be shown at the top of a chat
+function getChatHeader(email) {
+  return db.query(
+    "SELECT first_name, img_path FROM USERS WHERE email = ?;",
+    email
+  );
+}
+// returns fn, ln, username, email, bio and img path from users table
+function getProfile(id) {
+  return db.query(
+    "SELECT first_name, last_name, login_id, email, bio, img_path FROM USERS WHERE id = ?;",
+    id
+  );
+}
+// delete a user from the users table
+function deleteUser(id) {
+  return db.query("DELETE FROM users WHERE id= ?", id);
+}
+//delete a chat from chats table
+function deleteChat(id) {
+  return db.query(("DELETE FROM chats WHERE id=?", id));
 }
 
-function insertTask(priority, info, due) {
-    if (priority === '') {
-        priority = 'primary'
-    }
-    // no due? set to 7 days from now
-    if (due === '') {
-        due = moment().add(7, 'days').format('YYYY-MM-DD')
-    }
-    console.log(' inserting task data: ', { priority, info, due })
-    return db.query('INSERT INTO tasks SET ? ',
-        { priority, info, due })
-}
+// function getList(criteria = {}) {
+//     return db.query('SELECT * FROM tasks ' + (criteria ? 'WHERE ? ' : ''), criteria)
+// }
 
-function updateTask(id, priority, info, due) {
-    return db.query('UPDATE tasks SET ? WHERE id=?',
-        [{ priority, info, due }, id])
-}
+// function insertTask(priority, info, due) {
+//     if (priority === '') {
+//         priority = 'primary'
+//     }
+//     // no due? set to 7 days from now
+//     if (due === '') {
+//         due = moment().add(7, 'days').format('YYYY-MM-DD')
+//     }
+//     console.log(' inserting task data: ', { priority, info, due })
+//     return db.query('INSERT INTO tasks SET ? ',
+//         { priority, info, due })
+// }
 
-function deleteTask(id) {
-    return db.query('DELETE FROM tasks WHERE id=?', [id])
-}
+// function updateTask(id, priority, info, due) {
+//     return db.query('UPDATE tasks SET ? WHERE id=?',
+//         [{ priority, info, due }, id])
+// }
+
+// function deleteTask(id) {
+//     return db.query('DELETE FROM tasks WHERE id=?', [id])
+// }
 
 module.exports = {
-    getList, insertTask, updateTask, deleteTask
-}
+  getAllUsers,
+  getChatHeader,
+  getProfile,
+  deleteUser,
+  deleteChat,
+};
