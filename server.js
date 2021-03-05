3dc296d6e739b3dcd6ef85c9ba4f49b69fc000cc
John Murphy <murphbot55@gmail.com>
2021-03-05 10:34:21 -0500

    making profile page interact with db + edit

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const path = require('path');
 const http = require('http');
 const express = require('express');
 const socketio = require('socket.io');
+const Qs = require('Qs')
+const ormfnct = require('./app/models/orm')
 const { userInfo } = require('os');
 const PORT = process.env.PORT || 8080;
 const formatMessage = require('./utils/messages');
@@ -11,8 +13,8 @@ const {
   userLeave,
   getRoomUsers,
   addFriend,
+  getRoomReciever,
 } = require("./utils/users");
-const { getRoomReciever } = require("../html101/tutorials/realtimechatTutorial/utils/users.js");
 const { getMemChat } = require("./app/models/orm");
 
 const orm = require( './app/models/orm')
@@ -54,6 +56,29 @@ app.post('/public/login', async function (req, res) {
     var password = req.body.password;
     var result = orm.checkUser(username,password)
     res.redirect('./public/mainroom.html')
+})
+
+//edit profile
+app.get('/getProfile/:username', async function (req,res){
+  const username = req.params.username
+  //console.log('made it here', username)
+  const userData = await ormfnct.getProfile(username)
+  //console.log(userData)
+  res.send(userData)
+})
+
+app.post('/editProfile/:username', async (req, res)=>{
+  const userdata = req.body
+  let inputData = {
+            first_name: req.body.first_name,
+            last_name: req.body.last_name,
+            bio: req.body.bio,
+            email: req.body.email,
+            age: req.body.age,
+            username: req.body.username,
+        };
+  await ormfnct.updateUser(inputData)
+  console.log('User updated')
 })
 
 let friends = []
@@ -164,12 +189,12 @@ io.on('connection', (socket) => {
 
     });
 
-  });
+ 
   // console.log(`${socket.id} user connected`);
   // console.log(`user id: `,socket.id);
 
   //When a chat is sent
-  socket.on("chat message", async (msg) => {
+socket.on("chat message", async (msg) => {
     const user = getCurrentUser(socket.id);
     // --------- database work  and calls ------------------------
     let data = formatMessage(`${user.username}`, msg);
@@ -183,7 +208,7 @@ io.on('connection', (socket) => {
     ormfnct.saveMsg(data.username, data.text, data.time, `${users}`);
 
     //gets all messages from a specific user
-    const allMessages = await ormfnct.allMesagesFromUser(user);
+    //const allMessages = await ormfnct.allMesagesFromUser(user);
 
     // gets all messsages between two users 
     membersChat = await getMemChat(`${users}`)
