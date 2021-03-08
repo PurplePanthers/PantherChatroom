e2bebd02837a731c3c50c5e95bb223b92e56a976
John Murphy <murphbot55@gmail.com>
2021-03-08 00:21:45 -0500

    zzzz lets pray

--- app/models/orm.js
+++ app/models/orm.js
@@ -1,366 +1,136 @@
-const path = require('path');
-const http = require('http');
-const express = require('express');
-const socketio = require('socket.io');
-// const Qs = require('Qs')
-const ormfnct = require('./app/models/orm')
-const { userInfo } = require('os');
-const PORT = process.env.PORT || 8080;
-const formatMessage = require('./utils/messages');
-const {
-    userJoin,
-    getCurrentUser,
-    userLeave,
-    getRoomUsers,
-    addFriend,
-    getRoomReciever,
-} = require('./utils/users');
-const { getMemChat } = require('./app/models/orm');
+/* eslint-disable curly */
+/* eslint-disable no-redeclare */
+/* eslint-disable eqeqeq */
+/* eslint-disable camelcase */
+/* ORM FOLDER ========================================
+We abstract our database and information-modelling code
+into this section
+====================================================== */
+require('dotenv').config();
+const db = require('../config/connection.js')(
+    process.env.NAME,
+    process.env.DB_PASS
+);
+// an external npm package we are using
+const moment = require('moment');
+function isNotActive(username) {
+    return db.query(
+        `UPDATE users SET is_active="0" WHERE username ="${username}" `
+    );
+}
 
-const orm = require( './app/models/orm')
-const app = express();
-const server = http.createServer(app);
-const io = socketio(server);
-app.use(express.urlencoded({ extended: true }))
-app.use(express.json())
+function isActive(username) {
+    return db.query(
+        `UPDATE users SET is_active="1" WHERE username ="${username}" `
+    );
+}
 
-// Set static folder
-app.use(express.static(path.join(__dirname, 'public')));
+function getFriends(username) {
+    return db.query(`SELECT friend FROM matches WHERE username = "${username}" `);
+}
 
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + 'index.html');
-});
-app.get('/register', async function (req, res) {
-    var users = await orm.getAllUsers()
-    console.log('[Get] All users')
-    res.send(users)
-});
-// to store user input detail on post request
-app.post('/register', async function (req, res) {
+function addFriend(username, friend, randomRoomNumber) {
+    return db.query(
+        `INSERT INTO matches SET username="${username}", friend="${friend}", friend_room="${randomRoomNumber}";`
+    );
+}
 
-    const userData = req.body
-    console.log(userData)
-    const result = await orm.addUser(userData)
-    console.log('[Post] ', userData, result)
+// returns all users
+function getAllUsers() {
+    return db.query('SELECT * FROM USERS');
+}
+// this info is what will be shown at the top of a chat
+function getChatHeader(email) {
+    return db.query(
+        `SELECT first_name, img_path FROM USERS WHERE email ="${email}";`
+    );
+}
 
-    res.redirect('/login.html')
-})
-//Login
-app.get('/public/login', async function (req, res) {
-    var users = await orm.getAllUsers()
-    console.log('[Get] All users')
-    res.send(users)
-});
-app.post('/public/login', async function (req, res) {
-    var username = req.body.email_address;
-    var password = req.body.password;
-    var result = orm.checkUser(username,password)
-    res.redirect('./public/mainroom.html')
-})
+function updateUser(data) {
+    return db.query(
+        `UPDATE users SET first_name="${data.first_name}", last_name="${data.last_name}", bio="${data.bio}", email="${data.email}", age="${data.age}", img_path="${data.img_path}" WHERE username ="${data.username}" `
+    );
+}
 
-//edit profile
-app.get('/getProfile/:username', async function (req,res){
-    const username = req.params.username
-    //console.log('made it here', username)
-    const userData = await ormfnct.getProfile(username)
-    //console.log(userData)
-    res.send(userData)
-})
-app.get('/getStatus/:username', async function (req,res){
-    const username = req.params.username
-    const status = await ormfnct.checkStatus(username)
-    console.log('[status]', status)
-    res.send(status)
-})
+function saveMsg(username, msg, time, mems) {
+    return db.query(
+        `INSERT INTO chats SET chat_name="${username}",messages="${msg}", time="${time}", chat_members="${mems}";`
+    );
+}
 
-app.post('/editProfile/:username', async (req, res)=>{
-    const userdata = req.body
-    let inputData = {
-        first_name: req.body.first_name,
-        last_name: req.body.last_name,
-        bio: req.body.bio,
-        email: req.body.email,
-        age: req.body.age,
-        username: req.body.username,
-        img_path: req.body.pic,
-    };
-    console.log(inputData)
-    await ormfnct.updateUser(inputData)
-    console.log('User updated')
-})
+function getMemChat(user1, user2) {
+    return db.query(
+        `SELECT * FROM CHATS WHERE chat_members = "${user1}" || chat_members = "${user2}"  `
+    );
+}
+// add new user to db
+async function addUser(data) {
+    var sql = 'INSERT INTO users SET ?';
+    return db.query(sql, data);
+}
 
-let friends = []
-// Set static folder
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + 'index.html');
-});
-
-// THERE IS 3 WAYS TO SEND MESSAGES:
-// socket.emit()  ---> for the user
-// socket.broadcast.emit() ---> for everyone except that user
-// io.emit() ---> for everyone
-let randomRoomsArray = []
-let roomsMonitoring =[]
-let room = 1
-io.on('connection', (socket) => {
-    // console.log('testing rooms',io.rooms)
-    socket.emit('show friends', friends)
-    socket.on('joined room', ( username) => {
-        // room++
-        let isActive = true
-        ormfnct.isActive(username);
-        // room = 1
-        console.log(room)
-        roomsMonitoring.push(room)
-        console.log('[MonitoringRooms',roomsMonitoring)
-        let trying = roomsMonitoring.filter((v) => (v === room)).length
-        console.log('[trying]',trying)
-        if(trying >2){
-            room++
-            roomsMonitoring =[]
-            console.log('now roomsMonitoring is' ,roomsMonitoring)
-            console.log('now room number is' ,room)
-        }
-        socket.on('send common room',commonRoom=>{
-            room = commonRoom
-        })
-        const user = userJoin(socket.id, username, room, isActive)
-        console.log(user)
-        socket.join(user.room)
-        console.log('trying to find out the user room',user.room)
-        socket.emit('new room', user.room)
-        //to welcome user
-        socket.emit('message', formatMessage('PantherBot', 'Welcome to PantherChatroom!'));
-
-        // Broadcast when a user connects
-        socket.broadcast.to(user.room).emit('message', formatMessage('PantherBot', `${user.username} has joined the chat`));
-
-        //Send users and room info
-        io.to(user.room).emit('room users', {
-            room: user.room, users: getRoomUsers(user.room)
-        })
-    });
-
-    //Made a different one for texting friends in different rooms than the random ones
-    socket.on('joined friend room', async ({username, secondUserName})=>{
-        let isActive = true
-        ormfnct.isActive(username);
-        // room = 1
-        console.log('[room ]',room)
-        roomf = await ormfnct.getRoomName(username,secondUserName)
-        console.log('[room again]',roomf[0].friend_room)
-        const user = userJoin(socket.id, username, roomf[0].friend_room, isActive)
-        console.log(user)
-        socket.join(user.room)
-        console.log('trying to find out the user room',user.room)
-        socket.emit('new room', user.room)
-        //to welcome user
-        socket.emit('message', formatMessage('PantherBot', 'Welcome to PantherChatroom!'));
-
-        // Broadcast when a user connects
-        socket.broadcast.to(user.room).emit('message', formatMessage('PantherBot', `${user.username} has joined the chat`));
-
-        //Send users and room info
-        io.to(user.room).emit('room users', {
-            room: user.room, users: getRoomUsers(user.room)
-        })
-    });
-
-    // console.log(`${socket.id} user connected`);
-    // console.log(`user id: `,socket.id);
-
-    //Get who is online
-    // socket.emit('friend online', () => {
-    //     console.log('this user is active now', username)
-    //     let isActive = true
-    //     const user = userJoin(socket.id, username, room, isActive)
-    //     console.log(user)
-    // })
-
-    // When a chat is sent
-    // socket.on('chat message', (msg) => {
-    //     const user = getCurrentUser(socket.id);
-    //     io.to(user.room).emit('chat message', formatMessage(user.username, msg));
-    // });
-
-    socket.on('sendUsername', async (username) => {
-        let friends = await ormfnct.getFriends(username);
-
-        socket.emit('showFriends', friends);
-    });
-
-    socket.on('added', () => {
-        const user = getCurrentUser(socket.id);
-        socket.broadcast.to(user.room).emit('added', formatMessage(`${user.username}`, ' has accepted your friend request!'));
-        console.log('added :', getRoomUsers(user.room))
-        let user1 = getRoomUsers(user.room)[0].username
-        let user2 = getRoomUsers(user.room)[1].username
-        let randomRoomNumber = Math.round(Math.random() * 100000)
-        ormfnct.addFriend(`${user1}`, `${user2}` , randomRoomNumber);
-        ormfnct.addFriend(`${user2}`, `${user1}` , randomRoomNumber);
-
-        console.log(`${user1} and ${user2} are now friends`)
-        friends.push({ user1, user2 })
-        console.log('friends list', friends)
-    })
-    socket.on('rooms details', (room) => {
-        randomRoomsArray.push(room)
-        let filteredRandomRoom = randomRoomsArray.filter(function (item, pos, self) {
-            return self.indexOf(item) === pos;
-        })
-        console.log('array after[filteredRandomRoom]', filteredRandomRoom)
-        randomRoomsArray.forEach(room => {
-            newRoom = randomRoomsArray.filter((v) => (v === room)).length
-            if (newRoom === 1) {
-                availableRoom = room
-                console.log('tracking',availableRoom)
-                socket.emit('join this room', availableRoom)
-            }else{
-                availableRoom = Math.round(Math.random() * 100000)
-            }
-            //Was doing thissssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss
-            // socket.emit('one person room',availableRoom)
-        })
-        console.log('[availableRoom]', filteredRandomRoom)
-        // allRoomsArray.forEach(room=>{
-        //     console.log(`eye${room.room}`)
-        // })
-        socket.emit('rooms details', randomRoomsArray)
-    })
-
-    //When User disconnects
-    socket.on('disconnect', () => {
-        // console.log('user disconnected');
-        const user = userLeave(socket.id);
-        if (user) {
-            io.to(user.room).emit('message', formatMessage('PantherBot', `${user.username} has left the chat`));
-            io.to(user.room).emit('room users', {
-                room: user.room, users: getRoomUsers(user.room)
-            })
-            console.log(`testing room number ${randomRoomsArray[0]}`)
-            // we get the rooms from databse later on to see if they still have users aftersomeone leave
-            console.log('array before[randomRoomsArray]', randomRoomsArray)
-            let filteredRandomRoom = randomRoomsArray.filter(function (item, pos, self) {
-                return self.indexOf(item) === pos;
-            })
-            console.log('array after[filteredRandomRoom]', filteredRandomRoom)
-            filteredRandomRoom.forEach(room => {
-                console.log('Room:', room)
-                let x = getRoomUsers(room)
-                console.log('The users in this room', x)
-                if (x < 1) {
-                    console.log(`room ${room} is empty , need to be deleted from database`)
-                }
-            })
-            console.log('The user that left and needs "isActive" to be changed to false in database is:', user)
-            user.isActive = false
-            ormfnct.isNotActive(user.username);
-            console.log(user)
-
-            // let x = getRoomUsers(filteredRandomRoom)
-            // console.log(x)
-            // x.forEach(user=>{
-            //     console.log('users inside first room',user)
-            // })
-            // if (x === 1){
-            //     console.log('There is one user in the room',filteredRandomRoom[0])
-            // }
-        }
-
-    });
-
-
-    // console.log(`${socket.id} user connected`);
-    // console.log(`user id: `,socket.id);
-
-
-    socket.on('friend message',async(msg)=>{
-        console.log('this msg is coming like this :',msg)
-        const user = getCurrentUser(socket.id);
-        console.log ('[userrrrrrr]',user)
-        // --------- database work  and calls ------------------------
-        let data = formatMessage(`${user.username}`, msg.msg);
-
-        //creating a unique field on both usernames to retrieve their chat from db
-        // let reciever = getRoomUsers(`${user.room}`);
-        let users = [user.username,msg.user2]
-        // reciever.forEach((person)=> users.push(person.username))
-
-        // saves messages between two users
-        ormfnct.saveMsg(data.username, data.text, data.time, `${[user.username,msg.user2]}`);
-
-        //gets all messages from a specific user
-        //const allMessages = await ormfnct.allMesagesFromUser(user);
-
-        // gets all messsages between two users
-        // membersChat = await getMemChat(`${users}`)
-        // console.log(membersChat);
-        //console.log(`room: );
-
-        //-------------------------------------------------------
-        // io.to(user.room).emit('chat message', formatMessage(user.username, msg.msg));
-    })
-
-    //When a chat is sent
-    socket.on('chat message', async (msg) => {
-        const user = getCurrentUser(socket.id);
-        // --------- database work  and calls ------------------------
-        let data = formatMessage(`${user.username}`, msg);
-
-        //creating a unique field on both usernames to retrieve their chat from db
-        let reciever = getRoomUsers(`${user.room}`);
-        let users = []
-        reciever.forEach((person)=> users.push(person.username))
-
-        // saves messages between two users
-        // ormfnct.saveMsg(data.username, data.text, data.time, `${users}`);
-
-        //gets all messages from a specific user
-        //const allMessages = await ormfnct.allMesagesFromUser(user);
-
-        // gets all messsages between two users
-        membersChat = await getMemChat(`${users}`)
-        console.log(membersChat);
-        //console.log(`room: );
-
-        //-------------------------------------------------------
-        io.to(user.room).emit('chat message', formatMessage(user.username, msg));
-    });
-
-    socket.on('add friend', () => {
-        const user = getCurrentUser(socket.id);
-        socket.broadcast.to(user.room).emit('add friend', formatMessage(`${user.username}`, 'wants to Add you as friend'));
-        socket.emit('add sent', formatMessage(`${user.username}`, 'You have sent a friend request'));
-
-    })
-
-    socket.on('getMessages', async (mems) => {
-        console.log('find out what that is',mems)
-        const user1 = mems[0] + ',' + mems[1];
-        const user2 = mems[1] + ',' + mems[0];
-        const membersChat = await getMemChat(`${user1}`, `${user2}`);
-        socket.emit('pastMessages', membersChat);
-        console.log(membersChat);
-    });
-
-    //When User disconnects
-    socket.on('disconnect', () => {
-    // console.log('user disconnected');
-        const user = userLeave(socket.id);
-        if (user) {
-            io.to(user.room).emit(
-                'message',
-                formatMessage('PantherBot', `${user.username} has left the chat`)
-            );
-            io.to(user.room).emit('room users', {
-                room: user.room,
-                users: getRoomUsers(user.room),
+// })
+// }
+function checkUser(username, password) {
+    var sql = 'SELECT * FROM users WHERE email_address =? AND password =?';
+    db.query(sql, [username, password], function (err, data, fields) {
+        if (err) throw err;
+        if (data.length > 0) {
+            req.session.loggedinUser = true;
+            req.session.emailAddress = emailAddress;
+            res.redirect('/dashboard');
+        } else {
+            res.render('login-form', {
+                alertMsg: 'Your Email Address or password is wrong',
             });
         }
     });
+}
 
-});
+function checkStatus(username){
+    return db.query(
+        `SELECT is_active FROM users WHERE username = "${username}";`
+    );
+}
 
-server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+// returns fn, ln, username, email, bio and img path from users table
+function getProfile(username) {
+    return db.query(
+        `SELECT first_name, last_name, age, email, bio, img_path FROM USERS WHERE username = "${username}";`
+    );
+}
+function getFriendRoom(username) {
+    return db.query('SELECT chat_members FROM');
+}
+function getRoomName(username, user2) {
+    return db.query(
+        `SELECT friend_room FROM matches WHERE username = "${username}" and friend = "${user2}" ||username = "${user2}" and friend = "${username}";`
+    );
+}
+
+// delete a user from the users table
+function deleteUser(id) {
+    return db.query('DELETE FROM users WHERE id= ?', id);
+}
+//delete a chat from chats table
+function deleteChat(id) {
+    return db.query(('DELETE FROM chats WHERE id=?', id));
+}
+
+module.exports = {
+    getAllUsers,
+    getChatHeader,
+    getProfile,
+    deleteUser,
+    deleteChat,
+    addUser,
+    saveMsg,
+    getMemChat,
+    updateUser,
+    getFriends,
+    addFriend,
+    isActive,
+    isNotActive,
+    getRoomName,
+    checkStatus,
+};
--- public/js/user.js
+++ public/js/user.js
@@ -4,18 +4,17 @@ async function addUser() {
         first_name: document.querySelector('#first_name').value,
         last_name: document.querySelector('#last_name').value,
         username: document.querySelector('#username').value,
-        password: document.querySelector('#password').value,
-        confirm_password: document.querySelector('#confirm_password').value,
+        login_pw: document.querySelector('#password').value,
         security_question: document.querySelector('#security_question').value,
         security_answer: document.querySelector('#security_answer').value
     }
+    let confirm_password= document.querySelector('#confirm_password').value
     console.log(inputData)
     // eslint-disable-next-line eqeqeq
-    if (inputData.confirm_password != inputData.password) {
+    if (confirm_password != inputData.login_pw) {
         alert('Password & Confirm Password have to match');
         return false;
-    }
-
+    }    
     req = {
         method: 'POST',
         headers: {
@@ -26,7 +25,8 @@ async function addUser() {
     }
     console.log(req)
     await fetch('/register', req).then(r => r.json())
-
+    console.log('blahhhh')
+    window.document.location = '/login.html'
     // check unique username
     // if (inputData.confirm_password != inputData.password) {
     //     console.log('Password & Confirm Password have to match');
